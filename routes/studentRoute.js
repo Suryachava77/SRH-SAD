9548724cc1613e8e0475a0bd06a52fc1e2fa2c90
suryachava <suryachava77@gmail.com>
2026-04-23 02:49:13 +0530

    second class

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -6,5 +6,32 @@ const { verifyRole, restrictStudentToOwnData } = require("./auth/util");
 const { ROLES } = require("../../consts");
 
 const router = express.Router();
+//create student
+//name , email , password
+router.post("/", async (req, res) => {
+    const { name, email, password } = req.body;
 
+    if (!name || !email || !password) {
+        return res
+        .status(400)
+        .json({ message: "please provide name, email or password" });
+    }
+    //check if the student exists
+    try {
+        const existingstudent = await Student.findOne({ email });
+        if (existingStudent) {
+            return res
+            .status(400)
+            .json({ messege: "Student Already exists with this email" });
+        }
+        const newStudent = new Student({name, email, password});
+        const savedStudent = await newStudent.save();
+        res.status(201).json(savedStudent);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+
+
+
+}
 module.exports = router;
